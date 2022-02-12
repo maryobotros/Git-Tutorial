6dd9b772a7aa400fbae492defb97359224540eab
mmb1214 <78535223+mmb1214@users.noreply.github.com>
2022-02-12 12:56:29 -0800

    Added an if statement file

---
+++ IfStatement.java
@@ -0,0 +1,18 @@
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		int x = 10;
+
+		if(x > 10){
+			System.out.println("x is bigger than 10");
+		}
+		else{
+			System.out.println("x is less than or equal to 10");
+		}
+
+		System.out.println("hello");
+
+	}
+
+}
